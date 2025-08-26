4ba10b4436f3ca71c7e8613bfaeec1ba40e7cdf8
Sangogade Ayomide <ayomideoluwagade@gmail.com>
2025-08-26 22:58:36 +0100

    Completed lesson 4 - HTML, CSS review; Completed lesson 5 - Variables

---
+++ Strings/exercise.js
@@ -0,0 +1,5 @@
+console.log(`Items (${2+2}): $${(2095+2095+799+799)/100}`);
+console.log(`Shipping & handling: $${(499+499)/100}`)
+console.log(`Total before tax: $${(2095+2095+799+799+499+499)/100}`)
+console.log(`Estimated tax (10%): $${Math.round((2095+2095+799+799+499+499)*0.1)/100}`)
+//terminal strings - multi-line strings, interpolation
