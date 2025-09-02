5ca84f41e4aa5c41e1be2e713c423fabd7a3599d
Sangogade Ayomide <ayomideoluwagade@gmail.com>
2025-09-02 23:16:40 +0100

    Completed lesson 9 - Document Object Model

--- DOM (Document Object Model)/exercise.js
+++ DOM (Document Object Model)/exercise.js
@@ -1,13 +1,17 @@
 const product = {
     name: "Shirt",
-    price: 2095,
-    "delivery-time": "1 day",
+    price: 19.23,
+    'delivery-time': '1 day',
     ratings: {
         stars: 4.5,
         counts: 87
-    }
-}
+    },
+    fun: (text) => console.log(text)
+};
+
+product.fun(product.name);
 const productString = JSON.stringify(product);
 console.log(JSON.parse(productString));
-const { stars } = product.ratings;
-console.log(stars);
+
+const { price } = product;
+console.log(price);
