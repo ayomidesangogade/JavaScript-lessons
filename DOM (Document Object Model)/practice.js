a8607ce1e821443166f7182d3d3b1fb1fe54b463
Sangogade Ayomide <ayomideoluwagade@gmail.com>
2025-09-07 23:33:12 +0100

    Practice

--- DOM (Document Object Model)/practice.js
+++ DOM (Document Object Model)/practice.js
@@ -1,15 +1,19 @@
 const product = {
-    name: 'Shirt',
-    price: 12.00,
+    name: "Shirt",
+    price: 256,
     ratings: {
-        counts: 87,
-        stars: 4.5
+        stars: 4.5,
+        counts: 87
     },
-    fun: () => console.log('This is a function inside an object')
-};
-product.price = 14.49;
-product.fun();
-console.log(product);
+    fun: () => console.log('I love JavaScript')
+}
 
-const { price } = product;
-console.log(price);
+product.fun();
+delete product.price;
+console.log(product);
+product.price = 1024;
+console.log(product)
+const { stars } = product.ratings;
+console.log(stars);
+const productString = JSON.stringify(product);
+console.log(JSON.parse(productString));
