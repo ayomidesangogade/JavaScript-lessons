cdfa9429e94814e684902a0aac2c9693c735a8f8
Sangogade Ayomide <ayomideoluwagade@gmail.com>
2025-09-13 22:19:57 +0100

    Completed the exercises for lesson 12

--- Advanced Functions/practice.js
+++ Advanced Functions/practice.js
@@ -1,39 +1,8 @@
-setTimeout(() => {
-    console.log('LOL');
-}, 3000);
+const array = ['wash dishes', 'sleep', 'read'];
+array.forEach(function (element, index) {
+    console.log(element, index);
+})
 
-const product = {
-    name: 'Shirt',
-    price: 2065,
-    ratings: {
-        stars: 4.5,
-        counts: 87
-    },
-    fun: function () { console.log('I love JavaScript!') }
-}
-delete product.name
-console.log(product);
-product.fun();
-product.name = 'Shirt';
-console.log(product);
-
-const { price } = product;
-const object = {
-    price
-};
-console.log(object);
-
-const array = [1, 2, 3];
-array[3] = 6;
-console.log(array);
-const [element] = array;
-console.log(element);
-console.log(Array.isArray(array));
-array.splice(0, 2);
-console.log(array);
-const array2 = array.slice();
-console.log(array2);
-
-console.log(JSON.stringify(object));
-
-// setInterval(function () { console.log('Fuck you nigga!') }, 1000);
+setTimeout(function () {
+    console.log('Hello World!');
+}, 2000)
