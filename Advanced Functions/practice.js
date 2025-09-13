ea3deffc5e5971571e08ab7f8addd78ddf5bd1bf
Sangogade Ayomide <ayomideoluwagade@gmail.com>
2025-09-14 00:01:19 +0100

    I learned - Functions are values - setTimeout(), setInterval() - forEach() - Arrow functions, .addEventListener() - .filter(), .map() - Closure

--- Advanced Functions/practice.js
+++ Advanced Functions/practice.js
@@ -1,8 +1,11 @@
 const array = ['wash dishes', 'sleep', 'read'];
 array.forEach(function (element, index) {
+    if (element === 'sleep') {
+        return;
+    }
     console.log(element, index);
 })
 
-setTimeout(function () {
-    console.log('Hello World!');
-}, 2000)
+// setTimeout(function () {
+//     console.log('Hello World!');
+// }, 2000)
--- Advanced Functions/scripts/12-rock-paper-scissors.js
+++ Advanced Functions/scripts/12-rock-paper-scissors.js
@@ -38,9 +38,12 @@ function updateScoreElement() {
 let isAutoPlaying = false;
 let intervalId;
 
+// const autoPlay = () => {
+
+// }
 function autoPlay() {
     if (!isAutoPlaying) {
-        intervalId = setInterval(function () {
+        intervalId = setInterval(() => {
             const playerMove = pickComputerMove();
             playGame(playerMove);
         }, 1000);
@@ -50,6 +53,28 @@ function autoPlay() {
         isAutoPlaying = false;
     }
 }
+
+document.querySelector('.js-rock-button').addEventListener('click', () => {
+    playGame('rock');
+});
+
+document.querySelector('body').addEventListener('keydown', (event) => {
+    if (event.key === 'r' || event.key === 'R') {
+        playGame('rock');
+    } else if (event.key === 'p' || event.key === 'P') {
+        playGame('paper');
+    } else if (event.key === 's' || event.key === 'S') {
+        playGame('scissors');
+    }
+});
+
+document.querySelector('.js-paper-button').addEventListener('click', () => {
+    playGame('paper');
+});
+
+document.querySelector('.js-scissors-button').addEventListener('click', () => {
+    playGame('scissors');
+});
 
 function playGame(move) {
     const computerMove = pickComputerMove();
